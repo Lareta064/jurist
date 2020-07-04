12fbf8e1ef6f2bb4e3d8349f0262409d1a6c1d23
Lareta <lareta064@gmail.com>
2020-07-04 23:54:28 +0300

    faq-desktop

--- src/js/main.js
+++ src/js/main.js
@@ -154,4 +154,14 @@ $(document).ready(function () {
 	$(".team-slider-prev").click(function () {
 		teamSlider.trigger("prev.owl.carousel");
 	});
+
+	//аккордеон развернуть стрелку
+	$('.collapsable').on('show.bs.collapse', function () {
+		let tabIcon = $("#" + $(this).attr("aria-labelledby")).find(".arrow");
+		tabIcon.addClass("rotate");
+	});
+	$('.collapsable').on('hide.bs.collapse', function () {
+		let tabIcon = $("#" + $(this).attr("aria-labelledby")).find(".arrow");
+		tabIcon.removeClass("rotate");
+	});
 })
